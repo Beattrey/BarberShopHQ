8dbc2b02e61a55952dcd52af2cf9eb9a9811594d
beattrey <beattrey1@gmail.com>
2021-09-28 17:30:41 +0300

    Added save customers visit and contact form

--- app.rb
+++ app.rb
@@ -13,6 +13,40 @@ end
 class Barber < ActiveRecord::Base
 end
 
+class Contact < ActiveRecord::Base
+end
+
 get '/' do
+  @barbers = Barber.order "created_at DESC"
 	erb :index
+end
+
+get '/visit' do
+  @barbers = Barber.all
+  erb :visit
+end
+
+post '/visit' do
+  @name = params[:name]
+  @phone = params[:phone]
+  @date = params[:date]
+  @barber = params[:barber]
+
+  c = Client.new :name => @name, :phone => @phone, :datestamp => @date, :barber => @barber
+  c.save
+
+  erb "Вы успешно записались!"
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+post '/contacts' do
+  @email = params[:email]
+  @message = params[:message]
+
+  Contact.create :email => @email, :message => @message
+
+  erb "Ваше сообщение успешно отправлено"
 end
